f198dfa5c396d63a78423ab3f9b651a95d21e031
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2016-11-29 22:59:33 +0100

    several changes

--- twitter.py
+++ twitter.py
@@ -15,30 +15,26 @@ access_secret = ''
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
+
 class MyListener(StreamListener):
     def on_data(self, data):
-        jsondata=json_loads_byteified(data)
-        if jsondata['place']:
-            pass
-            pdb.set_trace()
-        elif jsondata['user']['location']:
-            pdb.set_trace()
 
-        else:
-            jsondata.update({"country":"undefined"})
+
+        #print (data)
+        jsondata=json_loads_byteified(data)
+        self.get_tweet(jsondata)
         DB = HashtagMDB()
         DB.insertHashtag(jsondata)
-        print (jsondata)
-        print "*****************************************"
+
+        #print (jsondata)
+        #print "*****************************************"
 
         def on_error(self, status):
             print(status)
             return True
 
-    def translate(self, word):
-        gs = goslate.Goslate()
-        pdb.set_trace()
-        gs.translate(word, 'en')
 
+
+GEOBOX_GERMANY =  [-180,-90,180,90]
 twitter_stream = Stream(auth, MyListener())
-twitter_stream.filter(track=['#SaturaTelemarketing'])
+twitter_stream.filter(track=["#BIGBellTest","#TheBIGBelltest","BIGBellTest","TheBIGBelltest"])
